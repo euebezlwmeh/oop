3185c0b4557b519b7756f3d5d724f2643018939e
Ksenia Chernova <gwoso@yandex.ru>
2025-10-22 01:39:22 +0500

    заполнение структуры случайными точками, функция для вывода значений вектора, ввод данных в main

--- 1lab/1.cpp
+++ 1lab/1.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 #include <random>
+#include <vector>
+#include <cmath>
 
 struct point3d
 {  
     double x,y,z;
 
-    point3d(double x=0.0, double y=0.0, double z=0.0)            ///< Конструктор по умолчанию
-    {
-        x = x;
-        y = y;
-        z = z;
-    }           
-    void print() const { std::cout << x << y << z << "\n"; }     ///< Выводит на экран местоположение частиц
-    double getBackX() const { return x; }                        ///< Возращает координату X
-    double getBackY() const { return y; }                        ///< Возращает координату Y
-    double getBackZ() const { return z; }                        ///< Возращает координату Z
+    point3d(double x = 0.0, double y = 0.0, double z = 0.0) : 
+        x(x), y(y), z(z) {}                                         ///< Конструктор по умолчанию
+
+    void print() const {
+         std::cout << "(" << x << ", " << y << ", " << z << ")\n";  ///< Выводит на экран местоположение частиц
+    }
+
+    double getBackX() const { return x; }                           ///< Возращает координату X
+    double getBackY() const { return y; }                           ///< Возращает координату Y
+    double getBackZ() const { return z; }                           ///< Возращает координату Z
 };
 
 class Figure
@@ -22,50 +24,93 @@ class Figure
     private:
         double R;
         double r;
-        double fi;
-        double psi;
+        double fi = M_PI;
+        double psi = M_PI;
+        int K;
 
-    static std::mt19937& getGenerator() {
-        static std::random_device rd;
-        static std::mt19937 gen(rd());
-        return gen;
-    }
+        static std::mt19937& getGenerator() {
+            static std::random_device rd;
+            static std::mt19937 gen(rd());
+            return gen;
+        }
 
     public:
 
-        //параметрическое уравнение для рисования тора
-        double x = (R + r * cos(psi)) * cos(fi);
-        double y = (R + r * cos(psi)) * sin(fi);
-        double z = r * sin(psi);
+        std::vector<point3d> points_arr;
 
-        Figure(double R=3.0, double r=1.0)
-        {
-            R = R;
-            r = r;
+        double getX(double fi, double psi) const {
+            return (R + r * cos(psi)) * cos(fi);
+        }
+        
+        double getY(double fi, double psi) const {
+            return (R + r * cos(psi)) * sin(fi);
+        }
+        
+        double getZ(double psi) const {
+            return r * sin(psi);
         }
 
-        static point3d point_fill(double x, double y, double z)
+        Figure(double R = 3.0, double r = 1.0) : R(R), r(r) {}
+
+        point3d random_points_filling()
         {
-            point3d point;
+            double x = getX(fi, psi);
+            double y = getY(fi, psi);
+            double z = getZ(psi);
             
             std::uniform_real_distribution<double> xDist(-x, x);
             std::uniform_real_distribution<double> yDist(-y, y);
             std::uniform_real_distribution<double> zDist(0, z);
 
-            point.x = xDist(getGenerator());
-            point.y = yDist(getGenerator());
-            point.z = zDist(getGenerator());
-            
+            double x_point = xDist(getGenerator());
+            double y_point = yDist(getGenerator());
+            double z_point = zDist(getGenerator());
+
+            return point3d(x_point, y_point, z_point);
+        }
+        
+        void filling_K(int K)
+        {
+            for (int i = 0; i < K; i++)
+            {
+                points_arr.push_back(point3d(random_points_filling()));
+            }
         }
 
-    int rnd()
-    {
-        
-    }
+        float rnd(int K)
+        {
+            point3d::getBackX;
+            point3d::getBackY;
+            point3d::getBackZ;
+
+            points_arr.push_back(point3d(random_points_filling()));
+        }
+
+        void printPoints() const {
+            std::cout << "Points in vector (" << points_arr.size() << " points):\n";
+            for (const auto& point : points_arr) {
+                point.print();
+            }
+        }
     
 };
 
 int main()
 {
+    int K;
+    double R;
+    double r;
+
+    std::cout << "Enter K\n";
+    std::cin >> K;
+    std::cout << "Enter R\n";
+    std::cin >> R;
+    std::cout << "Enter r\n";
+    std::cin >> r;
+
+    Figure fig(R, r);
+    fig.filling_K(K);
+    fig.printPoints();
+
     return 0;
 }
